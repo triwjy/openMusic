71691b5ba6edced31c7fc666214e36f24af5b8d0
triwjy <ztriwijaya@gmail.com>
2021-06-20 10:26:18 +0800

    SongsService and songs handler

---
+++ src/api/songs/index.js
@@ -0,0 +1,1 @@
+// This plugin is responsible for any request to /songs url
---
+++ src/services/inMemory/SongsService.js
@@ -0,0 +1,82 @@
+const { nanoid } = require('nanoid');
+
+// This service is responsible to manage resources (CRUD) in memory (array)
+class SongsService {
+  constructor() {
+    this._songs = [];
+  }
+
+  addSong({ title, year, performer, genre, duration }) {
+    const id = nanoid(16);
+    const createdAt = new Date().toISOString();
+    const updatedAt = createdAt;
+
+    const newSong = {
+      id, title, year, performer, genre, duration, createdAt, updatedAt,
+    };
+
+    this._songs.push(newSong);
+
+    const isSuccess = this._songs.filter((song) => song.id === id).length > 0;
+
+    if (!isSuccess) {
+      throw new Error('Failed to add song');
+    }
+
+    return id;
+  }
+
+  getSongs() {
+    const songData = [];
+    this_songs.forEach((song) => {
+      songData.push({
+        'id': song.id,
+        'title': song.title,
+        'performer': song.performer,
+      });
+    });
+    return songData;
+  }
+
+  getSongById(id) {
+    const song = this._songs.filter((s) => s.id === id)[0];
+
+    if (!song) {
+      throw new Error('Song not found');
+    }
+
+    return song;
+  }
+
+  editSongById(id, { title, year, performer, genre, duration } ) {
+    const index = this._songs.findIndex((s) => s.id === id);
+
+    if (index === -1) {
+      throw new Error('Song not found');
+    }
+
+    const updatedAt = new Date().toISOString();
+
+    this._songs[index] = {
+      ...this._songs[index],
+      title,
+      year,
+      performer,
+      genre,
+      duration,
+      updatedAt,
+    };
+  }
+
+  deleteNoteById(id) {
+    const index = this._songs.findIndex((song) => song.id === id);
+
+    if (index === -1) {
+      throw new Error('Song not found');
+    }
+
+    this._songs.splice(index, 1);
+  }
+}
+
+module.exports = SongsService;
