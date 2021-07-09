cfde3f51a9b0bd47168be0779652d9b03dff50a0
triwjy <ztriwijaya@gmail.com>
2021-07-09 14:08:28 +0800

    authentications plugin

--- src/server.js
+++ src/server.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 require('dotenv').config();
 
 const Hapi = require('@hapi/hapi');
@@ -12,9 +13,16 @@ const users = require('./api/users');
 const UsersService = require('./services/postgres/UsersService');
 const UsersValidator = require('./validator/users');
 
+// authentications
+const authentications = require('./api/authentications');
+const AuthenticationsService = require('./services/postgres/AuthenticationsService');
+const TokenManager = require('./tokenize/TokenManager');
+const AuthenticationsValidator = require('./validator/authentications');
+
 const init = async () => {
   const songsService = new SongsService();
   const usersService = new UsersService();
+  const authenticationsService = new AuthenticationsService();
 
   const server = Hapi.server({
     port: process.env.PORT,
@@ -39,6 +47,15 @@ const init = async () => {
       options: {
         service: usersService,
         validator: UsersValidator,
+      },
+    },
+    {
+      plugin: authentications,
+      options: {
+        authenticationsService,
+        usersService,
+        tokenManager: TokenManager,
+        validator: AuthenticationsValidator,
       },
     },
   ]);
